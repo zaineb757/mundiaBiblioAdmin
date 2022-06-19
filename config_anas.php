a6c827ec51a67bbfde175bdbaf7f79b42873f7ee
Anas SAMII <samiianas000@gmail.com>
2022-06-19 21:52:11 +0100

    anas

---
+++ config_anas.php
@@ -0,0 +1,14 @@
+<?php
+
+    define('DB_SERVER', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))');
+    define('DB_USERNAME', 'anassamii');
+    define('DB_PASSWORD', 'anas123');
+    define('DB_NAME', 'XE');
+
+    try{
+        $link = new PDO("oci:dbname=".DB_SERVER,DB_USERNAME,DB_PASSWORD);
+    } catch (PDOException $e) {
+        echo ($e->getMessage());
+    }
+    
+?>
